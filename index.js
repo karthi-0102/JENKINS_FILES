fd9c832c967664326425f294c65f71ef206d669e
karthi-0102 <karthikeyan.r@rently.com>
2024-05-24 14:34:33 +0530

    test case

---
+++ index.js
@@ -0,0 +1,12 @@
+class Test{
+    constructor(){
+        this.x =10;
+    }
+
+    update(){
+        this.x++;
+    }
+
+}
+
+module.exports = new Test();
---
+++ test/indexSpec.js
@@ -0,0 +1,13 @@
+const sinon = require('sinon')
+const test = require('../index.js');
+
+describe('index.js',()=>{
+
+    describe('update',()=>{
+        it('should update x',()=>{
+            test.update();
+           // sinon.assert(Index.x).to.be.equals(1);
+        })
+    })
+
+})
